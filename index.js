108e4810e8cdfdf63a53f7c2e4f043fb5ea76fb3
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-07-25 16:57:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var recipes = {eggs :3}
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  recipes = Object.assign(object,{[key]:value})
-  return recipes
-}
 
 function updateObjectWithKeyAndValue(object, key, value) {
   recipes = Object.assign({},object,{[key]:value})
