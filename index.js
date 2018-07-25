830c0456430cf5aaa36559dd0edc8837ad4fb3f8
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-07-25 16:23:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   recipes = Object.assign(object,{[key]:value})
   return recipes  
 }
+
+function deleteFromObjectByKey(object, key) {
+  delete recipes
+}
