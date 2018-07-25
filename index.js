f3d8d081d4d1f16627832cfea3068253bedc9633
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-07-25 16:49:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete Object.assign({}, object, [key])
+ recipes = delete Object.assign({}, object, [key])
   return recipes
 }
